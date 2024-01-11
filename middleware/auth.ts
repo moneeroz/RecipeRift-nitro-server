392201c887812058851b0f6f1c094c4ea462f407
Moneer <moneer.alromimah@gmail.com>
2024-01-11 12:15:06 -0700

    add auth middleware

---
+++ middleware/auth.ts
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+export default defineEventHandler((event) => {
+  const path = getRequestURL(event).pathname;
+  const isFavourites = path.startsWith("/favourites");
+  const isCartPath = path.startsWith("/cart");
+  const isAuthPutPath = path.startsWith("/auth") && event.method === "PUT";
+
+  if (isFavourites || isCartPath || isAuthPutPath) {
+    const token = event.headers.get("authorization");
+
+    if (!token) {
+      return Response.json("You are not authorized", { status: 401 });
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      event.context.user = decoded;
+    } catch (error) {
+      return Response.json("Invalid token", { status: 403 });
+    }
+  }
+});

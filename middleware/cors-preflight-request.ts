616daa9455113187bc5e614784c86660e695f027
Moneer <moneer.alromimah@gmail.com>
2024-01-11 15:55:41 -0700

    delete

--- middleware/cors-preflight-request.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler((event) => {
-  if (isPreflightRequest(event)) {
-    event.node.res.statusCode = 204;
-    event.node.res.statusMessage = "No Content";
-    return "OK";
-  }
-  event.headers.set("Access-Control-Allow-Origin", "*");
-  event.headers.set("Access-Control-Allow-Methods", "*");
-});

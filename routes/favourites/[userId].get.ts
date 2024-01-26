187816217178b1e2ad2d5c8c46c80303a6341e98
Moneer <moneer.alromimah@gmail.com>
2024-01-26 01:53:11 -0700

    clean up the response

--- routes/favourites/[userId].get.ts
+++ routes/favourites/[userId].get.ts
@@ -4,10 +4,13 @@ export default defineEventHandler(async (event) => {
   const { userId } = event.context.params;
 
   try {
-    const favourites = await prisma.favourite.findMany({
+    const data = await prisma.favourite.findMany({
       where: { userId },
       include: { recipe: { include: { recipeIngredients: true } } },
     });
+
+    const favourites = data.map((i) => i.recipe);
+
     return Response.json(favourites, { status: 200 });
   } catch (error) {
     return error.message;
